63aa61323782a9719802fa8bc71eb525d6b31c2d
Jesus <jesushuarancca27@gmail.com>
2022-10-04 14:21:26 -0500

    entregable 2 del proyecto

---
+++ src/routes/user.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const userSchema = require("../models/user");
+
+const router = express.Router();
+
+//crear usuario
+router.post('/users', (req, res) =>{
+    const  user = userSchema(req.body);
+    user
+    .save()
+    .then((data) => res.json(data))
+    .catch((error)=> res.json({message: error}));
+});
+
+//obtener todos los usuarios
+router.get('/users', (req, res) =>{
+    userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error)=> res.json({message: error}));
+});
+
+
+module.exports = router;
